974ed3154657430d28f4e21f62ad6f51b72c5432
Jai5112008 <88813528+Jai5112008@users.noreply.github.com>
2021-08-19 08:53:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var box;
+function draw() {
+background(30);
+  if(keyDown(DOWN_ARROW))
+  {
+  background('red');
+  
+  }  
+}
+function setup() {
+   box = createSprite(200,200); 
+   
+   
+  
+}
